2f895f8379998c0b6c42f67002fc6a3d6360873f
Ömer Faruk Aydın <80628611+ofaydn@users.noreply.github.com>
2021-11-05 21:53:15 +0300

    Update cal.cpp

--- cal.cpp
+++ cal.cpp
@@ -1,23 +1,55 @@
-#include <iostream> 
-#include <string>
-#include <stdio.h>
-using namespace std; 
-// function prototypes
-void testing();
-//
+#include <iostream>
+using namespace std;
+int x[2];
+int y[2];
+int area[2];
+int totalx;
+int totaly;
+int finalArea;
+void
+print ()
+{
+  cout << "The composed vector is on " << finalArea <<
+    ". area with those axis projections: " << "(" << totalx << "," << totaly<< ").";
+}
+intmain (){
+  for (int c = 0; c < 2; c++){
+      cout << "Please enter x: ";
+      cin >> x[c];
+      cout << "Please enter y: ";
+      cin >> y[c];
+      cout << "Please enter area: ";
+      cin >> area[c];}
+  for (int i = 0; i < 2; i++){
+      if (area[i] == (2 or 3)){
+	  x[i] = -x[i];
+      }
+      else if (area[i] == (3 or 4)){
+	  y[i] = -y[i];
+      }
+  }
+  totalx = x[0] + x[1];
+  totaly = y[0] + y[1];
+  if ((totalx or totaly) == 0){
+      cout << "This vector has no area";}
+  else if (totalx > 0 and totaly > 0){
+      finalArea = 1;
+      print ();
+  }
+  else if (totalx < 0 and totaly > 0){
+      finalArea = 2;
+      print ();
+  }
+  else if (totalx < 0 and totaly < 0){
+      finalArea = 3;
+      print ();
+  }
+  else{
+      finalArea = 4;
+      print ();
+  }
+}
 
-int main()  
-{   
-    testing();
-    testing();
-    return 0;
-}
-void testing(){
-    static int say = 3;
-    ;
-    cout<<say<<endl;
-    ++say;
-}
 
 
 
