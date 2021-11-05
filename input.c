452e82343dde98513058a520c6a7d615f45f081c
Ömer Faruk Aydın <80628611+ofaydn@users.noreply.github.com>
2021-11-05 11:30:08 +0300

    Update input.c

--- input.c
+++ input.c
@@ -1,30 +1,22 @@
 #include <stdio.h>
 int PI = 3.14;
-void print ();			//if a function is not returning anything it will be a void
-double surf ();
-double srf;
+//void print ();			//if a function is not returning anything it will be a void
+void getR(double x);
+double value;
 int
 main ()
 {
-  double r;
-  printf ("Enter your radius: ");
-  scanf ("%lf", &r);
-  printf ("%lf", surf (r));
-
+    getR(3);
+    printf("%lf", &value);
 
 }
-
-double
-surf (double r)
-{
-  double srf = PI * r * r;
-  return srf;
-}
-
-void
-print ()
-{
-  printf ("The surface of circtle is = %lf\n", srf);
+void getR(double x){
+    double r;
+    printf("Please enter radious: ");
+    scanf("%lf", &r);
+    value =  r*r*PI;
+    
+    
 }
 //sayının basamakları içindeki olasılıkları kontrol ederek 25in katı olup olmadığını tespit edecek
 //eğer içindeki herhangi bir kombinasyon 25in katı ise o sayının başlangıç ve bitiş digitlerine göre diğer sayıları çıkarıcak 
